2d8864a44e6cc6dc45e32f2b8f1d76804f7f595a
Marco Miralles <marco.miralles@gmail.com>
2023-12-18 18:40:08 -0800

    cleaning up day 8

--- day08/main.cpp
+++ day08/main.cpp
@@ -7,29 +7,16 @@
 #include <iostream>
 #include <limits>
 #include <map>
+#include <numeric>
+#include <ranges>
 #include <sstream>
 #include <string>
 #include <tuple>
 #include <vector>
-#include <ranges>
-#include <numeric>
 
-using pos_t = size_t;
-
-bool done(pos_t pos) { return (pos & 0xFF) == 'Z'; }
+using pos_t = std::string;
 
 size_t lcm(size_t a, size_t b) { return a * b / std::gcd(a, b); }
-
-pos_t to_pos(std::string str)
-{
-    size_t pos = 0;
-    for (char c : str)
-    {
-        pos <<= 8;
-        pos |= c;
-    }
-    return pos;
-}
 
 int main()
 {
@@ -40,49 +27,38 @@ int main()
     std::map<pos_t, std::pair<pos_t, pos_t>> graph;
 
     std::string instructions;
-
     std::getline(file, instructions);
+
     file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     std::vector<std::pair<pos_t, size_t>> positions;
 
     while (std::getline(file, str))
     {
-        std::stringstream sstr(str);
-        std::string name, left, right;
-        sstr >> name;
-        sstr.ignore(4);
-        sstr >> left >> right;
-        left.pop_back();
-        right.pop_back();
-        graph.insert({{to_pos(name), {to_pos(left), to_pos(right)}}});
+        std::string name = str.substr(0, 3), left = str.substr(7, 3),
+                    right = str.substr(12, 3);
+        graph.emplace(name, std::pair<pos_t, pos_t>(left, right));
         if (name.back() == 'A')
         {
-            positions.push_back({to_pos(name), 0});
+            positions.emplace_back(name, 0);
         }
     }
 
-    for (auto& record : positions)
+    for (auto& [pos, distance] : positions)
     {
-        auto pos = record.first;
         size_t count = 0;
-        for (size_t i = 0; !done(pos); i++, count++)
+        for (size_t i = 0; pos.back() != 'Z'; i++, count++)
         {
             if (i == instructions.size())
             {
                 i = 0;
             }
-
-            if (instructions.at(i) == 'L')
-            {
-                pos = graph.at(pos).first;
-            }
-            else
-            {
-                pos = graph.at(pos).second;
-            }
+            pos = instructions.at(i) == 'L' ? pos = graph.at(pos).first
+                                            : pos = graph.at(pos).second;
         }
-        record.second = count+1;
+        distance = count + 1;
     }
-    std::cout << std::ranges::fold_right_last(positions | std::ranges::views::values, lcm).value();
+    std::cout << std::ranges::fold_right_last(
+                     positions | std::ranges::views::values, lcm)
+                     .value();
 }
